7535a10cee4a892c23ee83f11eec8b3c826a05e2
daoudomardz <daoudomardz@users.noreply.github.com>
2019-11-24 16:29:03 +0100

    Revert "lang switcher 3"
    This reverts commit b237699905361f35a30ca577c9db9d2af98505b3.

--- modules/Language/Views/frontend/switcher.blade.php
+++ modules/Language/Views/frontend/switcher.blade.php
@@ -30,8 +30,8 @@
                 @endif
             @endforeach --}}
             
-            <li><a href="https://www.bookingsaken.com/en"class="is_login"><span class="flag-icon flag-icon-gb"></span> English</a></li>
-            <li><a href="https://www.bookingsaken.com/ar"class="is_login"><span class="flag-icon flag-icon-sa"></span> العربية</a></li>
+            <li><a href="https://bookingsaken.com/en"class="is_login"><span class="flag-icon flag-icon-gb"></span> English</a></li>
+            <li><a href="https://bookingsaken.com/ar"class="is_login"><span class="flag-icon flag-icon-sa"></span> العربية</a></li>
             <li><a href="https://fr.bookingsaken.com/en"class="is_login"><span class="flag-icon flag-icon-fr"></span> Français</a></li>
             <li><a href="https://ru.bookingsaken.com/en"class="is_login"><span class="flag-icon flag-icon-ru"></span> Pусский</a></li>
             <li><a href="https://tr.bookingsaken.com/en"class="is_login"><span class="flag-icon flag-icon-tr"></span> Türk</a></li>
