e0c986f31597a2903454f8e9ed9576f4cb27fd40
daoudomardz <daoudomardz@users.noreply.github.com>
2019-11-25 09:31:13 +0100

    lang

--- modules/Language/Views/frontend/switcher.blade.php
+++ modules/Language/Views/frontend/switcher.blade.php
@@ -30,8 +30,8 @@
                 @endif
             @endforeach --}}
             
-            <li><a href="http://en.bookingsaken.com"class="is_login"><span class="flag-icon flag-icon-gb"></span> English</a></li>
-            <li><a href="http://arabic.bookingsaken.com"class="is_login"><span class="flag-icon flag-icon-sa"></span> العربية</a></li>
+            <li><a href="http://www.bookingsaken.com/en"class="is_login"><span class="flag-icon flag-icon-gb"></span> English</a></li>
+            <li><a href="http://www.bookingsaken.com/ar"class="is_login"><span class="flag-icon flag-icon-sa"></span> العربية</a></li>
             <li><a href="https://fr.bookingsaken.com"class="is_login"><span class="flag-icon flag-icon-fr"></span> Français</a></li>
             <li><a href="https://ru.bookingsaken.com"class="is_login"><span class="flag-icon flag-icon-ru"></span> Pусский</a></li>
             <li><a href="https://tr.bookingsaken.com"class="is_login"><span class="flag-icon flag-icon-tr"></span> Türk</a></li>
