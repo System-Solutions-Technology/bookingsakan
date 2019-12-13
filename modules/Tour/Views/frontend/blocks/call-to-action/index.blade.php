4c842da256bf3a420c06ace50b26bac4b2ec5364
daoudomardz <daoudomardz@users.noreply.github.com>
2019-12-13 16:16:30 +0100

    Update index.blade.php

--- modules/Tour/Views/frontend/blocks/call-to-action/index.blade.php
+++ modules/Tour/Views/frontend/blocks/call-to-action/index.blade.php
@@ -10,12 +10,17 @@
                         {{$sub_title}}
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-2">
                     @if($link_title)
                         <a class="btn-more" href="{{$link_more}}">
                             {{$link_title}}
                         </a>
                     @endif
+                </div>
+                <div class="col-md-2">
+                        <a class="btn-more" href="https://apps.apple.com/us/app/bookingsaken/id1489329775">
+                        iOS
+                        </a>
                 </div>
             </div>
         </div>
