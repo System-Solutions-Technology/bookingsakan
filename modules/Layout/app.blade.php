9f48721afae563a8b7d349b5b22fd5e0db4830fd
daoudomardz <daoudomardz@users.noreply.github.com>
2020-02-16 17:03:29 +0100

    Update app.blade.php

--- modules/Layout/app.blade.php
+++ modules/Layout/app.blade.php
@@ -61,7 +61,7 @@
             console.log("the switchers are ready, I can tweak them")
             console.log($( '.dropdown-menu-lang' ).html());
             
-            $( '.dropdown-menu-lang' ).html('<li><a href="http://bookingsaken.com/en" class="is_login"><span class="flag-icon flag-icon-gb"></span> English</a></li><li><a href="http://bookingsaken.com/ar"  class="is_login"><span class="flag-icon flag-icon-sa"></span> العربية</a></li><li><a href="https://fr.bookingsaken.com" class="is_login"><span class="flag-icon flag-icon-fr"></span> Français</a></li><li><a href="https://ru.bookingsaken.com" class="is_login"><span class="flag-icon flag-icon-ru"></span>  Pусский</a></li><li><a href="https://tr.bookingsaken.com" class="is_login"><span class="flag-icon flag-icon-tr"></span>  Türk</a></li>');
+            $( '.dropdown-menu-lang' ).html('<li><a href="www.bookingsaken.com/en" class="is_login"><span class="flag-icon flag-icon-gb"></span> English</a></li><li><a href="www.bookingsaken.com/ar"  class="is_login"><span class="flag-icon flag-icon-sa"></span> العربية</a></li><li><a href="fr.bookingsaken.com" class="is_login"><span class="flag-icon flag-icon-fr"></span> Français</a></li><li><a href="ru.bookingsaken.com" class="is_login"><span class="flag-icon flag-icon-ru"></span>  Pусский</a></li><li><a href="tr.bookingsaken.com" class="is_login"><span class="flag-icon flag-icon-tr"></span>  Türk</a></li>');
             console.log("after");
             console.log($( '.dropdown-menu-lang' ).html());
         })
