f5a14712386d319d4e5eb1c1b02676aded4175b5
daoudomardz <daoudomardz@users.noreply.github.com>
2020-03-24 16:13:50 +0100

    Update index.blade.php

--- modules/Tour/Views/frontend/blocks/call-to-action/index.blade.php
+++ modules/Tour/Views/frontend/blocks/call-to-action/index.blade.php
@@ -1,6 +1,6 @@
-<div class="bravo-call-to-action mobile-app-call">
+<div class="bravo-call-to-action  mobile-app-call" >
     <div class="container">
-        <div class="context">
+        <div class="context" style="background-color:white; border: 1px solid #d7dce3;border-radius: 3px;">
             <div class="row">
                 <div class="col-md-8">
                     <div class="title">
@@ -10,19 +10,28 @@
                         {{$sub_title}}
                     </div>
                 </div>
-                <div class="col-md-2">
-                    @if($link_title)
-                        <a class="btn-more" href="{{$link_more}}">
-                            {{$link_title}}
-                            <i class="fa fa-android" aria-hidden="true"></i>
-                        </a>
-                    @endif
-                </div>
-                <div class="col-md-2">
-                        <a class="btn-more" href="https://apps.apple.com/us/app/bookingsaken/id1489329775">
-                        iOS
-                        <i class="fa fa-apple" aria-hidden="true"></i>
-                        </a>
+                <div class="col-md-4">
+                    <div class="row d-flex justify-content-center">
+    
+                        <div class="mx-1">
+                            {{-- @if($link_title)
+                                <a class="btn-more" href="{{$link_more}}">
+                                    {{$link_title}}
+                                    <i class="fa fa-android" aria-hidden="true"></i>
+                                </a>
+                            @endif --}}
+                            <a class="btn-more btn-lg " style="background-color:#a4c639;color:white;"  href="https://apps.apple.com/us/app/bookingsaken/id1489329775">
+                                Android
+                                <i class="fa fa-android " aria-hidden="true" style="font-size:1.5em"></i>
+                            </a>
+                        </div>
+                        <div class="mx-1">
+                                <a class="btn-more btn-lg" style="background-color:#7D7D7D;color:white;" href="https://apps.apple.com/us/app/bookingsaken/id1489329775">
+                                iOS
+                                <i class="fa fa-apple "  aria-hidden="true" style="font-size:1.5em"></i>
+                                </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
