3903bcfb7ea892c34bfddd1fce2606bfb5b06202
ilyas28chenouf <46300676+ilyas28chenouf@users.noreply.github.com>
2020-05-26 14:59:46 +0300

    fix design

--- modules/Tour/Views/frontend/blocks/call-to-action/index.blade.php
+++ modules/Tour/Views/frontend/blocks/call-to-action/index.blade.php
@@ -1,8 +1,8 @@
 <div class="bravo-call-to-action  mobile-app-call" >
     <div class="container">
         <div class="context" style="background-color:white; border: 1px solid #d7dce3;border-radius: 3px;">
-            <div class="row">
-                <div class="col-md-8">
+            <div class="row" style="display:flex;justify-content:center;">
+                <div class="col-md-8" style="text-align:center;">
                     <div class="title">
                         {{$title}}
                     </div>
@@ -10,23 +10,23 @@
                         {{$sub_title}}
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-8">
                     <div class="row d-flex justify-content-center">
 
-                        <div class="mx-1">
+                        <div class="mx-1 col-xl-8 col-sm-12">
                             {{-- @if($link_title)
                                 <a class="btn-more" href="{{$link_more}}">
                                     {{$link_title}}
                                     <i class="fa fa-android" aria-hidden="true"></i>
                                 </a>
                             @endif --}}
-                            <a class="btn-more btn-lg "  href="https://apps.apple.com/us/app/bookingsaken/id1489329775">
-                              <img style="width:137px;"  src="images/android.png" alt="">
+                            <a class="btn-more btn-lg "  href="https://play.google.com/store/apps/details?id=com.bookingsaken.sstech">
+                              <img style="width:100%;"  src="images/android.png" alt="">
                             </a>
                         </div>
-                        <div class="mx-1">
+                        <div class="mx-1 col-xl-8 col-sm-12">
                                 <a class="btn-more btn-lg" href="https://apps.apple.com/us/app/bookingsaken/id1489329775">
-                              <img style="width:120px;" src="images/ios.png" alt="">
+                              <img style="width:100%;" src="images/ios.png" alt="">
                                 </a>
                         </div>
                     </div>
