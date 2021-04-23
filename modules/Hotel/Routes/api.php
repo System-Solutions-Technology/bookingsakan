b370d108b72b278c06f4d74bb89cbd6d8a0c7d00
Abdelhadi Naimi <naimi.abdelhadi.97@gmail.com>
2021-04-23 23:54:11 +0200

    AN : added hotel rooms route

--- modules/Hotel/Routes/api.php
+++ modules/Hotel/Routes/api.php
@@ -2,7 +2,8 @@
 use \Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>config('hotel.hotel_route_prefix')],function(){
-    Route::post('/','HotelController@index')->defaults('json', true); //
-    Route::post('/best','HotelController@getBestHotels')->defaults('json', true);;// Detail
-    Route::post('/{slug}','HotelController@detail')->defaults('json', true);;// Detail
+    Route::post('','HotelController@index')->defaults('json', true); //
+    Route::post('rooms','HotelController@checkAvailability');
+    Route::post('best','HotelController@getBestHotels')->defaults('json', true);;// Detail
+    Route::post('{slug}','HotelController@detail')->defaults('json', true);;// Detail
 });
